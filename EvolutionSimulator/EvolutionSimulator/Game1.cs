a6015a49647ba420580085ade909a4ddde5abce4
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2017-03-13 22:27:29 +0100

    Basic

---
+++ EvolutionSimulator/EvolutionSimulator/Game1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace EvolutionSimulator
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        SpriteFont FontDefault;
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            FontDefault = Content.Load<SpriteFont>("DefaultFont");
+        }
+        protected override void UnloadContent()
+        {
+        }
+        GameUpdate update = new GameUpdate();
+        protected override void Update(GameTime gameTime)
+        {
+            var state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Space))
+            {
+                update.StartThread();
+            }
+            if (state.IsKeyDown(Keys.S))
+            {
+                update.StopThread();
+            }
+            update.Invoke();
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            update.incrementFramtick();
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(FontDefault, "Update #" + update.UpdateTick, Vector2.Zero, Color.White);
+            spriteBatch.DrawString(FontDefault, "Frame #" + update.FrameTick, new Vector2(0, 20), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ EvolutionSimulator/EvolutionSimulator/GameUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolutionSimulator
+{
+    class GameUpdate : AbstractAsynchronousUpdate
+    {
+        public int UpdateTick { get; private set; }
+        public int FrameTick { get; private set; }
+
+        public void incrementFramtick()
+        {
+            FrameTick++;
+        }
+
+        protected override void Update()
+        {
+            UpdateTick++;
+        }
+    }
+}
