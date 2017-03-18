ebec164bdd460c4a3b6f4b955636cd957f92120e
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2017-03-18 15:22:16 +0100

    Removed testing items.

--- EvolutionSimulator/EvolutionSimulator/GameUpdate.cs
+++ EvolutionSimulator/EvolutionSimulator/GameUpdate.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-using System.Diagnostics;
 
 namespace EvolutionSimulator
 {
     class GameUpdate : AbstractAsynchronousUpdate
     {
         private World world;
-        Stopwatch time;
         public GameUpdate(World world, Game game) : base(game)
         {
             this.world = world;
-            time = new Stopwatch();
-            time.Start();
         }
 
         public ulong UpdateTick { get; private set; }
@@ -31,11 +27,6 @@ namespace EvolutionSimulator
         {
             UpdateTick++;
             world.Update(UpdateTick);
-            if (UpdateTick == 2147483647)
-            {
-                time.Stop();
-                int lol = 1;
-            }
         }
     }
 }
